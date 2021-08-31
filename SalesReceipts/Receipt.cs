168486ea5beb5c40772f16ca158aa2069edf2607
Alex Upton <alexander99w@ou.edu>
2021-08-31 18:43:26 -0500

    making methods

--- SalesReceipts/Receipt.cs
+++ SalesReceipts/Receipt.cs
@@ -27,5 +27,53 @@ namespace SalesReceipts
             CogPrice = 79.99;
             GearPrice = 250.00; //save 1
         }
+
+        public Receipt(int id, int cog, int gear)
+        {
+            CustomerID = 0;
+            CogQuantity = 0;
+            GearQuantity = 0;
+            SaleDate = DateTime.Now;
+            SalesTaxPercent = .089;
+            CogPrice = 79.99;
+            GearPrice = 250.00; //save 1
+        }
+
+        public double CalculateTotal()
+        {
+            double netAmount = CalculateNetAmount();
+
+            double taxAmount = CalculateTaxAmount();
+
+            double value = taxAmount + netAmount;
+            return value;
+        }
+
+        public void PrintReceipt()
+        {
+             //string receipt = $"Customer: 5" + 
+        }
+
+        private double CalculateTaxAmount()
+        {
+            return CalculateNetAmount() * SalesTaxPercent;
+        }
+
+        private double CalculateNetAmount()
+        {
+            double netAmount, cogPriceWMarkup, gearPriceWMarkup,
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
