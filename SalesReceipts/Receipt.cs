4b593bf6a81954a5632794875d5d443b752f76ce
Alex Upton <alexander99w@ou.edu>
2021-08-31 17:22:35 -0500

    Add project files.

---
+++ SalesReceipts/Receipt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesReceipts
+{
+    public class Receipt
+    {
+        public int CustomerID { get; set; }
+        public int CogQuantity { get; set; }
+        public int GearQuantity { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        private double SalesTaxPercent;
+        private double CogPrice;
+        private double GearPrice;
+
+
+
+        public Receipt()
+        {
+            CustomerID = 0;
+            CogQuantity = 0;
+            GearQuantity = 0;
+            SaleDate = DateTime.Now;
+            SalesTaxPercent = .089;
+            CogPrice = 79.99;
+            GearPrice = 250.00;
+        }
+    }
+}
