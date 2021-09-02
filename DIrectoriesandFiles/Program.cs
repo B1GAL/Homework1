1ff3ce9efee4182da125874adf6b115f641f96e6
Alex Upton <alexander99w@ou.edu>
2021-09-02 18:26:20 -0500

    done with directories and files but not with sales receipts

---
+++ DIrectoriesandFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace DIrectoriesandFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a directory path to get all file info");
+            string path = Console.ReadLine();
+
+            while (Directory.Exists(path) == false)
+            {
+                Console.WriteLine("Enter a path that exists");
+                path = Console.ReadLine();
+            }
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            foreach (var files in directoryInfo.GetFiles())
+            {
+                Console.WriteLine(files.Name);
+            }
+        }
+
+
+    }
+}
